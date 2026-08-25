44b2e88d0b29a656c67b973dc4aca0d1ebf42707
Thomas Krampl <85170275+thokra-nav@users.noreply.github.com>
2026-08-25 13:45:52 +0200

    Upgrade dependencies (#28)
    Co-authored-by: Christer Edvartsen <christer.edvartsen@nav.no>

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+    '^.+\\.ts$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.test.json' }],
   },
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
